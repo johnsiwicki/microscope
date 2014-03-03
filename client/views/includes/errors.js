cfdbc59411a464bacc545776836be5f12ffdee87
John Siwicki <johnsiwicki@gmail.com>
2014-03-02 19:52:52 -0500

    Created a form to submit comments

--- client/views/includes/errors.js
+++ client/views/includes/errors.js
@@ -6,3 +6,16 @@ throwError = function(message) {
 clearErrors = function() {
   Errors.remove({seen: true});
 }
+
+Template.errors.helpers({
+  errors: function() {
+    return Errors.find();
+  }
+});
+
+Template.error.rendered = function() {
+  var error = this.data;
+  Meteor.defer(function() {
+    Errors.update(error._id, {$set: {seen: true}});
+  });
+};
